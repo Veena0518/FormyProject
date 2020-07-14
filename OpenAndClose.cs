e58eb0929cf816152379fee929d592e662073f7b
Veena0518 <39529463+Veena0518@users.noreply.github.com>
2020-07-13 17:46:17 -0700

    Add files via upload

---
+++ OpenAndClose.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FormyProject
+{
+    public class OpenAndClose
+    {
+        private IWebDriver driver;
+
+        //Google url
+        private const string FormyUrl = @"http://formy-project.herokuapp.com/";
+
+        //open method
+        public IWebDriver Open(string type)
+        {
+            if (string.IsNullOrEmpty(type) || type == "chrome")
+            {
+                driver = new ChromeDriver();
+            }
+            driver.Navigate().GoToUrl(FormyUrl);
+            return driver;
+        }
+
+        //close method
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ TitleAndLogo.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormyProject
+{
+    public class TitleAndLogo
+    {
+        IWebDriver driver;
+
+        //private By title;
+        private By logo;
+
+        public IWebElement Logo { get { return this.driver.FindElement(logo); } }
+
+
+        //helper method to find elements
+        private void Init()
+        {
+            //title = By.XPath("");
+            logo = By.Id("logo");
+        }
+        public TitleAndLogo(IWebDriver driver)
+        {
+            this.driver = driver;
+            //calls Init method
+            this.Init();
+        }
+    }
+}
